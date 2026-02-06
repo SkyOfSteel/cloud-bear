7d184e06282d531a4da3456bcb30938034c2554f
SkyOfSteel <ivanzots@outlook.com>
2026-02-05 23:56:49 -0500

    Fixing argparse

--- main.py
+++ main.py
@@ -6,21 +6,20 @@ from print_s3_buckets import list_s3_buckets
 
 parser = ArgumentParser()
 parser.add_argument('-l', '--list', help='Lists the buckets that exist in your AWS account.', action='store_true')
-args: Namespace = parser.parse_args()
+args: Namespace = parser.parse_known_args()
 
 def main():
-    local_path = sys.argv[1]
-    bucket_name = sys.argv[2] if len(sys.argv) == 3 else BUCKET_NAME
-    # first we list - right now it lists everything even if the function catches an exception
-    
     if args.list:
         list_s3_buckets()
         sys.exit()
-    # then we upload - until I add extra CLI operators
-    upload_file_to_s3(local_path, bucket_name, object_key=None)
-
-if len(sys.argv) < 2 or len(sys.argv) > 3:
+    
+    if not args.list and (len(sys.argv) < 2 or len(sys.argv) > 3):
         print("Usage: python3 upload_to_s3.py <path_to_file> <bucket_name>")
         sys.exit(1)
+    
+    local_path = sys.argv[1]
+    bucket_name = sys.argv[2] if len(sys.argv) == 3 else BUCKET_NAME
+    
+    upload_file_to_s3(local_path, bucket_name, object_key=None)
 
 main()
